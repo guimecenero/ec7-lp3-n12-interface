ee8289c89d5917bf1c97c1bb7c6fe6f64816f9d8
vitor-gercov <gercovvitor@gmail.com>
2021-05-11 19:22:19 -0300

    branch bugada fodassi.

--- interface/src/app/app.module.ts
+++ interface/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +12,8 @@ import { DashboardComponent } from './views/pages/dashboard/dashboard.component'
 import { HomeComponent } from './views/pages/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ArtistListComponent } from './views/pages/artist/artist-list/artist-list.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -30,8 +32,11 @@ import { ArtistListComponent } from './views/pages/artist/artist-list/artist-lis
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    NgxChartsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {}
--- interface/src/app/views/pages/dashboard/dashboard.component.ts
+++ interface/src/app/views/pages/dashboard/dashboard.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  single: any[];
+  view: any[] = [500, 400];
+
+  // options
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
+
   constructor() { }
 
   ngOnInit(): void {
